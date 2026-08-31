bcdc21f122005cb6f72081724d5a3a3f61ae791b
dewx21 <dewanshpsinghwork@gmail.com>
2026-08-31 08:56:47 +0000

    sum of two numbers

--- first.c
+++ first.c
@@ -1,5 +1,17 @@
-#include<stdio.h>
-int main(){
-    printf("Enter two numbers");
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    int sum = a + b;
+    
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+    
     return 0;
 }
