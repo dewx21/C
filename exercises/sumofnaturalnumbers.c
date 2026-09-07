5a55de74e2b24bfbc4e8c3ebe83853dc3a8228b8
dewx21 <dewanshpsinghwork@gmail.com>
2026-09-07 09:31:14 +0000

    add program to display multiplication table of 2

--- exercises/sumofnaturalnumbers.c
+++ exercises/sumofnaturalnumbers.c
@@ -3,13 +3,16 @@
 int main()
 {
     int n;
-    int sum;
+    int sum = 0;
 
     printf("Enter n: ");
     scanf("%d", &n);
 
-    sum = n * (n + 1) / 2;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
     printf("Sum of the first %d natural numbers is %d\n", n, sum);
 
     return 0;
 }
+
